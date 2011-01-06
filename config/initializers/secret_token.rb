1a7fb5b289017df1f5f1102b549771d2558a3d8b
Drew Dara-Abrams <drew@13.sub-75-209-190.myvzw.com>
2011-01-05 21:25:50 -0800

    new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Littlegeodesignworld::Application.config.secret_token = 'f59f7fc45a92cb6fd8f14e5c34602f7b8c501941fe8724a9a2f2be911d500944fb4ff0efc4f3949a9aab3e114352539abce3c080258a9402ba0848334ea6513e'
